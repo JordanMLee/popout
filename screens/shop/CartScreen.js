cdd9219c8ef251f40e085e482cba714932ab2c05
Jordan Lee <jordan.m.lee01@gmail.com>
2019-12-24 23:56:49 -0500

    added template delete cart items functionality

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Button, FlatList, StyleSheet, Text, View} from 'react-native';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
+import * as cartActions from "../../store/actions/cart";
 
 const CartScreen = props => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -17,12 +18,12 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return transformedCartItems;
+        return transformedCartItems.sort((a, b) => a.productId > b.productId ? 1 : -1);
 
 
     });
 
-
+    const dispatch = useDispatch();
     return (
         <View style={styles.screen}>
             <View style={styles.summary}>
@@ -40,6 +41,7 @@ const CartScreen = props => {
                             title={itemData.item.productTitle}
                             amount={itemData.item.sum}
                             onRemove={() => {
+                                dispatch(cartActions.removeFromCart(itemData.item.productId));
                             }}
                         />
                     )}
